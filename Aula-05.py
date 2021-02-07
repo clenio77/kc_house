1d674636147e6eb99cb7d4d1acad564cfba36b73
clenio77 <mouraaf@hotmail.com>
2021-02-07 15:23:42 -0300

    upgrade aula-5

--- Aula-05.py
+++ Aula-05.py
@@ -3,25 +3,16 @@ import streamlit as st
 import plotly.express as px
 
 
-
 def main():
     #functions
-    # upload of data
-    def upload_data(x):
-        data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
-        if data is not None:
-            df = pd.read_csv(data)
-            st.dataframe(df.head())
+    @st.cache(allow_output_mutation=True)
+    def get_data(path):
+        df = pd.read_csv(path)
+        df['date'] = pd.to_datetime(df['date'])
+        return df
 
-        # read data
-        # @st.cache(allow_output_mutation=True)
-        # def get_data(path):
-        #     data = pd.read_csv(path)
-        #     # data['date'] = pd.to_datetime(data['date'])
-        #     return data
-        #
-        # # load data
-        # data = get_data('./datasets/dados.csv')
+    df = get_data('./datasets/dados.csv')
+
 
     # Categorizing the Level column
     def add_level(df):
@@ -37,7 +28,7 @@ def main():
 
     #Map interactive
     def map_interative(df):
-        #houses = df[['id', 'lat', 'long', 'price', 'level']].copy()
+        #houses = df[['id', 'lat', 'long', 'price', 'level']]
         fig = px.scatter_mapbox(houses,
                                 lat='lat',
                                 lon='long',
@@ -48,8 +39,8 @@ def main():
                                 zoom=10)
         fig.update_layout(mapbox_style='open-street-map')
         fig.update_layout(height=600, margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
-        #fig.show()
-        st.write(fig)
+        st.plotly_chart(fig)
+        #st.write(fig)
 
     activities = ["EDA", "Plots", "Questions"]
     choice = st.sidebar.selectbox("Select Activities", activities)
@@ -58,10 +49,12 @@ def main():
 
     if choice == 'EDA':
         st.subheader("**Exploratory Data Analysis**")
-        data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
-        if data is not None:
-            df = pd.read_csv(data)
-            st.dataframe(df.head())
+        #df = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
+        df = get_data('./datasets/dados.csv')
+        st.write(df.head())
+        # if data is not None:
+        #     df = pd.read_csv(data)
+        
 
         if st.sidebar.checkbox("Number Of Rows and Columns"):
             st.write('{} linhas  e  {} colunas.'.format(df.shape[0],df.shape[1]))
@@ -74,7 +67,7 @@ def main():
             st.write(df.describe())
 
         if st.sidebar.checkbox("Verify null rows"):
-            st.write(df.isnull().any())
+            st.write(df.isnull().sum())
 
         if st.sidebar.checkbox("Deleting null lines"):
             df.dropna()
@@ -91,20 +84,31 @@ def main():
 
     elif choice == 'Plots':
         st.subheader("Data Visualization Dashboard and Maps")
-        data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
-        if data is not None:
-            df = pd.read_csv(data)
-            st.dataframe(df.head())
-
+        st.write(df.head(3))
 
         if st.sidebar.checkbox("Interactive Map by Property Price"):
             st.subheader('**Map for Level!** ')
             houses = df[['id', 'lat', 'long', 'price', 'level']].copy()
-            map_interative(df)
+            #map_interative(houses)
+            st.write(houses.head(3))
+            fig = px.scatter_mapbox(
+                houses,
+                lat='lat',
+                lon='long',
+                color='level',
+                size='price',
+                color_continuous_scale=px.colors.cyclical.IceFire,
+                size_max=15,
+                zoom=10
+            )
+
+            fig.update_layout(mapbox_style='white-bg')
+            fig.update_layout(height=900, margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
+            st.plotly_chart(fig)
 
 
         if st.sidebar.checkbox("Adding Interactive Filters"):
-            houses = st.dataframe(df[['id', 'lat', 'long', 'price', 'level', 'is_waterfront']])
+            houses = df[['id', 'lat', 'long', 'price', 'level', 'is_waterfront']]
             #df['is_waterfront'] = df['waterfront'].apply(lambda x: 'yes' if x == 1 else 'no')
             # Select the type you want to view:
             selector = st.sidebar.radio(
@@ -121,7 +125,7 @@ def main():
 
         if st.sidebar.checkbox("Map for Price"):
             #is_check = st.sidebar.checkbox('Display Map for Price')
-            houses = df[['id', 'lat', 'long', 'price', 'level']].copy()
+            #houses = df[['id', 'lat', 'long', 'price', 'level']]
             price_min = int(df['price'].min())
             price_max = int(df['price'].max())
             price_avg = int(df['price'].mean())
@@ -132,46 +136,59 @@ def main():
                                              price_avg)
 
             houses = df[df['price'] < price_slider][['id', 'lat', 'long', 'price','level']]
-            #st.dataframe(houses)
-            map_interative(houses)
+            st.write(houses.head())
+            fig = px.scatter_mapbox(
+                houses,
+                lat='lat',
+                lon='long',
+                color='level',
+                size='price',
+                color_continuous_scale=px.colors.cyclical.IceFire,
+                size_max=15,
+                zoom=10)
+            fig.update_layout(mapbox_style='open-street-map')
+            fig.update_layout(height=600, margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
+            st.plotly_chart(fig)
+            # st.write(fig)
 
-        if st.sidebar.checkbox("Map for Number Bathrooms"):
+        if st.sidebar.checkbox("Map for Number Bedrooms"):
             #is_check = st.sidebar.checkbox('Display Map for Price')
-            houses = df[['id', 'lat', 'long', 'price', 'level','bathrooms']].copy()
-            bathrooms_min = int(df['bathrooms'].min())
-            bathrooms_max = int(df['bathrooms'].max())
-            bathrooms_avg = int(df['bathrooms'].mean())
+            houses = df[['id', 'lat', 'long', 'price', 'level','bedrooms']]
+            bedrooms_min = int(df['bedrooms'].min())
+            bedrooms_max = int(df['bedrooms'].max())
+            bedrooms_avg = int(df['bedrooms'].mean())
 
-            bathrooms_slider = st.sidebar.slider('Bathrooms range',
-                                             bathrooms_min,
-                                             bathrooms_max,
-                                             bathrooms_avg)
+            bedrooms_slider = st.sidebar.slider('Bedrooms range',
+                                             bedrooms_min,
+                                             bedrooms_max,
+                                             bedrooms_avg)
 
-            houses = df[df['bathrooms'] < bathrooms_slider][['id', 'lat', 'long', 'price','level','bathrooms']]
-            #st.dataframe(houses)
+            houses = df[df['bedrooms'] < bedrooms_slider][['id', 'lat', 'long', 'price','level','bedrooms']]
+            st.dataframe(houses.head())
             fig = px.scatter_mapbox(houses,
                                     lat='lat',
                                     lon='long',
                                     color='level',
-                                    size='bathrooms',
-                                    # color_continuous_scale=px.colors.cyclical.IceFire,
+                                    size='bedrooms',
+                                    color_continuous_scale=px.colors.cyclical.IceFire,
                                     size_max=15,
                                     zoom=10)
             fig.update_layout(mapbox_style='open-street-map')
             fig.update_layout(height=600, margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
-            # fig.show()
             st.write(fig)
+            #st.write(fig)
 
     elif choice == 'Questions':
         st.title("Questions")
-        data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
-        if data is not None:
-            df = pd.read_csv(data)
-            st.dataframe(df.head())
+        #df = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
+        df = get_data('./datasets/dados.csv')
+        # if df is not None:
+        #     df = pd.read_csv(df)
+        #     st.dataframe(df.head())
 
 
         if st.sidebar.checkbox("1. How many properties per level?"):
-            add_level(df)
+            #add_level(df)
             st.write('**Nivel 0:** preço entre R$ 0.00 e R$ 321.950 = **{}**'.format(df[df['level'] == 'nivel_0'].shape[0]))
             st.write('**Nivel 1:** preço entre R$ 321.950 e R$ 450.000 = **{}**.'.format(df[df['level'] == 'nivel_1'].shape[0]))
             st.write('**Nivel 2:** preço entre R$ 450.000 e R$ 645.000 = **{}**'.format(df[df['level'] == 'nivel_2'].shape[0]))
