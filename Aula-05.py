968339f3d559a8a0eafe897efafc0d2bb8298dc4
clenio77 <mouraaf@hotmail.com>
2021-02-07 15:39:51 -0300

    upgrade aula-5 styles

--- Aula-05.py
+++ Aula-05.py
@@ -100,7 +100,7 @@ def main():
                 zoom=10
             )
 
-            fig.update_layout(mapbox_style='white-bg')
+            fig.update_layout(mapbox_style='open-street-map')
             fig.update_layout(height=900, margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
             st.plotly_chart(fig)
 
